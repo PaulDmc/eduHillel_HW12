2340ceeb90f7b9be74ba984d80ab33323c1c6029
Pavlo <pavdemchnko@gmail.com>
2025-01-30 21:46:39 -0500

    Added new class with recursion and calculation of Fibonacci numbers

---
+++ FibonacciCalculator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eduHillel_HW12
+{
+    public static class FibonacciCalculator
+    {
+        public static int CalculateFibonacci(int numb)
+        {
+                if (numb < 0)
+                {
+                    throw new ArgumentException("The number must be non-negative.");
+                }
+
+                if (numb == 0) return 0;
+                if (numb == 1) return 1;
+
+                return CalculateFibonacci(numb - 1) + CalculateFibonacci(numb - 2);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+
+namespace eduHillel_HW12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int result;
+
+            Console.WriteLine("Enter number n: ");
+            if (int.TryParse(Console.ReadLine(), out int numb))
+            {
+                try
+                {
+                    result = FibonacciCalculator.CalculateFibonacci(numb);
+                    Console.WriteLine($"Number of Fibonacci {numb}: {result}");
+                }
+                catch (ArgumentException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Enter number.");
+            }
+            Console.ReadKey();
+        }
+    }
+}
